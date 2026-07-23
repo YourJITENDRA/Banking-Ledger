208cf70b404cf31d33ae4d1231f3c17af86d6db6
Jitendra Mishra <jitendramishra223355@gmail.com>
2026-07-23 23:31:01 +0530

    Initial commit: Banking System API

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,117 @@
+const userModel = require("../models/user.model")
+const jwt = require("jsonwebtoken")
+const emailService = require("../services/email.service")
+
+
+async function userRegisterController(req, res) {
+    try {
+        const { email, name, password } = req.body
+
+        if (!email || !name || !password) {
+            return res.status(400).json({
+                message: "Email, name, and password are required",
+                status: "Failed"
+            })
+        }
+
+        const isExist = await userModel.findOne({ email })
+
+        if (isExist) {
+            return res.status(422).json({
+                message: "User already exists with this email😒",
+                status: "Failed⚠️"
+            })
+        }
+
+        const user = await userModel.create({
+            email,
+            password,
+            name
+        })
+
+        const token = jwt.sign(
+            { userId: user._id },
+            process.env.JWT_SECRET,
+            { expiresIn: "3d" }
+        )
+
+        res.cookie("token", token)
+
+       
+
+        return res.status(201).json({
+            user: {
+                _id: user._id,
+                email: user.email,
+                name: user.name
+            },
+            token
+        }),  await emailService.sendRegistrationEmail(user.email, user.name)
+        
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message || "Internal server error",
+            status: "Failed"
+        })
+
+        
+    }
+
+    
+}
+
+
+async function userLoginController(req, res) {
+    try {
+        const { email, password } = req.body
+
+        if (!email || !password) {
+            return res.status(400).json({
+                message: "Email and password are required",
+                status: "Failed"
+            })
+        }
+
+        const user = await userModel.findOne({ email }).select("+password")
+
+        if (!user) {
+            return res.status(401).json({
+                message: "Email or password is invalid😒"
+            })
+        }
+
+        const isValidPassword = await user.comparePassword(password)
+
+        if (!isValidPassword) {
+            return res.status(401).json({
+                message: "Email or password is invalid😒😒"
+            })
+        }
+
+        const token = jwt.sign(
+            { userId: user._id },
+            process.env.JWT_SECRET,
+            { expiresIn: "3d" }
+        )
+
+        res.cookie("token", token)
+
+        return res.status(200).json({
+            user: {
+                _id: user._id,
+                email: user.email,
+                name: user.name
+            },
+            token
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message || "Internal server error",
+            status: "Failed"
+        })
+    }
+}
+
+module.exports = { userRegisterController, userLoginController }
+
+
---
+++ src/models/user.model.js
@@ -0,0 +1,45 @@
+const mongoose = require ("mongoose")
+const bcrypt = require("bcryptjs")
+
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: [true, "Email is required to create an user account"],
+        trim: true,
+        lowercase: true,
+        match: [/^[^\s@]+@[^\s@]+\.[^\s@]+$/, "Invalid Email address"],
+        unique: [true, "Email already exists"]
+    },
+    name: {
+        type: String,
+        required: [true, "Name is required for creating an user account"],
+
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required for creating an user account"],
+        minlength: [6, "Password must contain minimum of six characters"],
+        select: false
+    }
+}, {
+    timestamps: true
+})
+
+userSchema.pre("save", async function () {
+    if (!this.isModified("password")) {
+        return
+    }
+
+    const hash = await bcrypt.hash(this.password, 10)
+    this.password = hash
+})
+
+userSchema.methods.comparePassword = async function (password) {
+
+    return await bcrypt.compare(password, this.password)
+}
+
+const userModel = mongoose.model("user", userSchema)
+
+module.exports = userModel
