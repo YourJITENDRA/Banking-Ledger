0260ca81641a3c69305f4c695b9962a192abef14
Jitendra Mishra <jitendramishra223355@gmail.com>
2026-07-24 10:16:51 +0530

    Created accounts model

--- src/app.js
+++ src/app.js
@@ -1,17 +1,22 @@
 const express = require ("express")
 const cookieParser = require("cookie-parser")
+
+// Routes
 const authRouter = require("./routes/auth.routes")
+const accountRouter = require("./routes/account.routes")
 
 
 
 
 const app = express()
+
+
 app.use(express.json())
 app.use(cookieParser())
 
-
-
+// Use routes
 app.use("/api/auth", authRouter)
+app.use("/api/accounts", accountRouter)
 
 
 
