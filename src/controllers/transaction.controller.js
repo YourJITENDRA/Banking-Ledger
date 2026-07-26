9342592ad1c700be351be2e9ee97127a11877700
Jitendra Mishra <jitendramishra223355@gmail.com>
2026-07-26 21:52:36 +0530

    Added tranctionController, transactionRoute

---
+++ src/controllers/transaction.controller.js
@@ -0,0 +1,104 @@
+const transactionModel = require("../models/transaction.model")
+const ledgerModel = require("../models/ledger.model")
+const accountModel = require("../models/account.model")
+const emailService = require("../services/email.service")
+const mongoose = require("mongoose")
+
+/**
+ * - Create a new transaction
+ * THE 10-STEP TRANSFER FLOW:
+     * 1. Validate request
+     * 2. Validate idempotency key
+     * 3. Check account status
+     * 4. Derive sender balance from ledger
+     * 5. Create transaction (PENDING)
+     * 6. Create DEBIT ledger entry
+     * 7. Create CREDIT ledger entry
+     * 8. Mark transaction COMPLETED
+     * 9. Commit MongoDB session
+     * 10. Send email notification
+ */
+
+
+async function createTransaction(req, res){
+    const {fromAccount, toAccount, amount, idempotencyKey} = req.body;
+
+    if(!fromAccount || !toAccount || !amount || !idempotencyKey){
+        return res.ststus(400).json({
+            message: "FromAccount, ToAccount, Amount and IdempotencyKey sre required."
+        })
+    }
+
+
+
+    const fromUserAccount = await accountModel.findOne({
+        _id: fromAccount,
+
+    })
+
+    const toUserAccount = await accountModel.findOne({
+        _id: toAccount,
+
+    })
+
+
+    if (!fromUserAccount || !toUserAccount){
+        return res.status(400).json({
+            message: "Invalid fromAccount or toAccount"
+        })
+    }
+
+    // Validate Idempotency key
+
+    const isTransactionAlreadyExists = await transactionModel.findOne({
+        idempotenctKey: idempotencyKey
+    })
+
+    if(isTransactionAlreadyExists){
+        if(isTransactionAlreadyExists.status === "COMPLETED"){
+            return res.status(200).json({
+                message: "Transaction already processed.",
+                transaction: isTransactionAlreadyExists
+            })
+        }
+        if(isTransactionAlreadyExists.status === "PENDING"){
+            return res.status(200).json({
+            message: "Transaction is still procssing"
+            })
+        }
+        if(isTransactionAlreadyExists.status === "FAILED"){
+            return res.status(500).json({
+                message: "Transaction Failed, Please retry!"
+            })
+        }
+        if(isTransactionAlreadyExists.status === "REVERSED"){
+            return res.status(500).json({
+                message: "Transaction was reversed , Please retry!"
+            })
+        }
+    }
+
+
+    // Check account status
+
+
+    if (fromUserAccount.status !== "ACTIVE" || toUserAccount.status !== "ACTIVE"){
+        return res.status(400).json({
+            message: "Both fromAccount and toAccount must be be ACTIVE to process transactions."
+
+        })
+    }
+
+
+    // Derive sender balance from ledger
+
+
+    const balance = await fromUserAccount.getBalance()
+
+    if(balance < amount){
+        return res.status(400).json({
+            message: `Insufficient balance. Current balance is ${balance}. Requested amount is ${amount}`
+        })
+    }
+
+}
